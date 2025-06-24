fcccc33ffdef439bbd29883788dabe5a8e459a2b
parson-harness <todd.parson@harness.io>
2025-06-24 17:23:21 -0500

    updates to module testing

--- main.tf
+++ main.tf
@@ -1,19 +1,8 @@
-provider "azurerm" {
-  features {}
-}
-
-resource "azurerm_resource_group" "example" {
-  name     = "dns-test-rg"
-  location = "East US"
-}
-
 module "azure-private-dns-zone" {
   source  = "app.harness.io/EeRjnXTnS4GrLG5VNNJZUw/azure-private-dns-zone/azurerm"
   version = "v1.0.1"
 
-  name                = "privatelink.database.windows.net"
-  resource_group_name = azurerm_resource_group.example.name
-  tags = {
-    environment = "test"
-  }
+  name                = var.name
+  resource_group_name = var.resource_group_name
+  tags                = var.tags
 }
