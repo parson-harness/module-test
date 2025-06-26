0916add5e86422994ff8377da2872a75bc4d4cbf
parson-harness <todd.parson@harness.io>
2025-06-26 16:57:29 -0500

    adding versions.tf

---
+++ versions.tf
@@ -0,0 +1,10 @@
+terraform {
+  required_version = ">= 1.4.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.100.0" # adjust to match your compatibility
+    }
+  }
+}
