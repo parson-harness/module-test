cbd19cfc8b5d0bd008f2611db8483340cdd85aed
parson-harness <todd.parson@harness.io>
2025-06-26 22:33:25 -0500

    adding outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "dns_zone_id" {
+  value = module.azure-private-dns-zone.dns_zone_id
+}
+
+output "dns_zone_name" {
+  value = module.azure-private-dns-zone.dns_zone_name
+}
+
+output "module_version" {
+  value = module.azure-private-dns-zone.module_version
+}
