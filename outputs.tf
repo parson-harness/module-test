0f9b07480b11d6d21349612988880e53c1d533bc
parson-harness <108954591+parson-harness@users.noreply.github.com>
2025-06-29 12:02:00 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,10 @@ output "dns_zone_name" {
   value = module.azure-private-dns-zone.dns_zone_name
 }
 
-output "module_version" {
+output "dns_module_name" {
+  value = module.azure-private-dns-zone.module_name
+}
+
+output "dns_module_version" {
   value = module.azure-private-dns-zone.module_version
 }
