20641d91c728f4cc004d7665ae05f54db3416640
parson-harness <todd.parson@harness.io>
2025-06-29 21:16:08 +0000

    Bump module version to v1.0.6-alpha for validation

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "azurerm" {
 }
 module "azure-private-dns-zone" {
   source  = "app.harness.io/EeRjnXTnS4GrLG5VNNJZUw/azure-private-dns-zone/azurerm"
-  version = "v1.0.5"
+  version = "v1.0.6-alpha"
 
   name                = var.name
   resource_group_name = var.resource_group_name
